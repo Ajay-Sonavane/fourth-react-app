9c8b07ffb3209e2a2bb654e5cadf789c53f97116
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-16 12:13:29 +0530

    stateful list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
-      <h1>Props demo </h1>
-      {/* calling the messege */}
+      <h1>Stateful List</h1>
       <ListDemo />
     </>
   );
 }
 
 function ListDemo() {
-  let list = ["", "", ""];
-  let list1 = [];
-  for (let i = 0; i < 100; i++) {
-    list1.push("");
-  }
-  //
+  // let list = ["delhi"]
+  // C1: Stateful Variable
+  let [list, setList] = useState(["delhi"]);
+
+  // C2: Acton Member Funcn
+  let addItemAction = () => {
+    // why didn u use push method, push method update the same object
+    // setter method requires immutable object.
+    // We have to pass new list.
+    let newList = [...list, "mumbai"];
+    console.log(newList);
+
+    // DOM :: TRIGGER DOM
+    setList(newList);
+  };
 
   return (
     <>
-      {list.map((item) => (
-        <div>
-          <h1>Hello world</h1>
-        </div>
-      ))}
+      {/** C3: Event Binding */}
+      <input type="button" value="Add New Item" onClick={addItemAction} />
 
-      {list1.map((item) => (
-        <div>
-          <h1>Hello Universe</h1>
-        </div>
+      {/** C4: List */}
+      {list.map((item) => (
+        <h1>Hello {item}</h1>
       ))}
     </>
   );
