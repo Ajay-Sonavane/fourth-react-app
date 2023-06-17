2d46fe4cd19a3e382d86db97d14355102aac5722
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-17 08:48:07 +0530

    19

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import { useRef, useState } from "react";
+import MyTodo from "./pages/Mytodo";
+import MyRegistration from "./pages/MyRegistration";
 
 function App() {
   return (
     <>
-      <h1>My Todo</h1>
       <MyTodo />
+      <hr />
+      <MyRegistration />
     </>
   );
 }
 
-function MyTodo() {
-  let [todo, setTodo] = useState({ task: "", description: "" });
-
-  let handleChangeTaskAction = (e) => {
-    // console.log(e.target);
-    //e.targt===input object
-    let newTodo = { ...todo, task: e.target.value };
-    setTodo(newTodo);
-  };
-
-  let handleChageDescription = (e) => {
-    // console.log(e.target);
-    let newTodo = { ...todo, description: e.target.value };
-    setTodo(newTodo);
-  };
-
-  let addTodoAction = () => {
-    alert(todo.task + "" + todo.description);
-  };
-
-  return (
-    <>
-      <input
-        className="form-control"
-        type="text"
-        placeholder="Enter todo"
-        value={todo.task}
-        onChange={handleChangeTaskAction}
-      />
-
-      <textarea
-        className="form-control"
-        cols="30"
-        placeholder="Enter description"
-        rows="3"
-        value={todo.description}
-        onChange={handleChageDescription}
-      ></textarea>
-      <input type="button" value="Add Todo" onClick={addTodoAction} />
-    </>
-  );
-}
 export default App;
