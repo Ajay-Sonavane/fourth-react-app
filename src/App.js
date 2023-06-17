20cd09644610aa1b901c313aa203417d574371e8
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-17 09:32:11 +0530

    Links are now as component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import { useRef, useState } from "react";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavigationLinks />
         <Routes>
           <Route path="/" element={<h1>Hello world</h1>} />
           <Route path="/home" element={<h1>Home</h1>} />
@@ -14,6 +15,16 @@ function App() {
           <Route path="/registration" element={<MyRegistration />} />
         </Routes>
       </BrowserRouter>
+    </>
+  );
+}
+
+function NavigationLinks() {
+  return (
+    <>
+      <Link to={"/home"}> Home |</Link>
+      <Link to={"/todo"}>Todo |</Link>
+      <Link to={"/registration"}>Registration </Link>
     </>
   );
 }
